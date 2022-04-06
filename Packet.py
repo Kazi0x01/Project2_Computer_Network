5dac7a81f03e6dfc2e0f9b30399b90ff6f902191
muditgarg48 <muditgarg48@gmail.com>
2022-04-06 17:33:44 +0100

    Successful working implementation of RDT3.0 and CRC

--- Packet.py
+++ Packet.py
@@ -1,72 +1,48 @@
 import pickle 
 from hashlib import sha1
+from socket import IP_TOS
 from string import hexdigits
 
-from matplotlib.colors import hexColorPattern
+# from matplotlib.colors import hexColorPattern
 # from termcolor import colored
 
-
-
-class Packet:
-    def __init__(self, status, data, ack, seq_num): 
-        self.status = status
-        self.data = data
+class Packet_reciever:
+    def __init__(self, ack, seq_num): 
         self.seq_num = seq_num
         self.ack = ack
-        self.checksum = sha1(data).hexdigest()
-    
+        self.type = 'has_ack'
+        self.checksum = sha1(ack).hexdigest()
               
-    
-    def dumps(self):
-        return pickle.dumps(self)
+    def check(self):
+        return self.checksum == sha1(self.ack).hexdigest()
 
-
+class Packet_sender:
+    def __init__(self, data, seq_num, ip, port): 
+        self.data = data
+        self.seq_num = seq_num
+        self.ip = ip
+        self.port = port
+        self.type = 'has_data'
+        self.checksum = sha1(data).hexdigest()    
+              
     def check(self):
         return self.checksum == sha1(self.data).hexdigest()
-    
 
-    def get(self, field):
-        if field == 'seq_num':
-            return str(self.seq_num)
-        elif field == 'data':
-            return self.data
-        elif field == 'status':
-            return self.status
-        elif field == 'ack':
-            return self.ack
-        elif field == 'checksum':
-            return self.checksum
+# if __name__ == '__main__':
+#     msg = "Helloo I am data"
+#     msg = msg.encode('utf-8')
 
-    # def printf(self):
-    #     if self.pkt.ack == 'ACK':
-    #         print(colored('Positive Ack ' + self.pkt.seq_num, color= 'green'))
-    #     elif self.pkt.ack == 'NAK':
-    #         print(colored('Negative Ack ' + self.pkt.seq_num, color='red'))
-    #     elif self.pkt.status == 'found':
-    #         print(colored('File if found, recieving', color = 'green'))
-    #     elif self.pkt.status == 'not_found':
-    #         print(colored('File is not found', color = 'red'))
-    #     else:
-    #         print(colored('Recieved Packet ' + self.pkt.seq_num, color = 'yellow'))
+#     pk = Packet("found", msg, 'ACK', 1)
+#     print("Status : {}".format(pk.status))
+#     print("Ack : {}".format(pk.ack))
+#     print("seq_num : {}".format(pk.seq_num))
+#     print("data : {}".format(pk.data))
+#     print("checksum : {}".format(pk.checksum))
+#     print("******CHECK SUM ******")
+#     cal_checksum = pk.check()
+#     print("Claculated sum : {}".format(sha1(pk.data.decode('utf-8')).hexdigest()))
 
-
-if __name__ == '__main__':
-    msg = "Helloo I am data"
-    msg = msg.encode('utf-8')
-
-    pk = Packet("found", msg, 'ACK', 1)
-    print("Status : {}".format(pk.status))
-    print("Ack : {}".format(pk.ack))
-    print("seq_num : {}".format(pk.seq_num))
-    print("data : {}".format(pk.data))
-    print("checksum : {}".format(pk.checksum))
-    print("******CHECK SUM ******")
-    cal_checksum = pk.check()
-    print("Claculated sum : {}".format(sha1(pk.data.decode('utf-8')).hexdigest()))
-
-    if cal_checksum:
-        print("data not corrupted")
-    else:
-        print("corrupted !!")
-
-    
+#     if cal_checksum:
+#         print("data not corrupted")
+#     else:
+#         print("corrupted !!")
--- client.py
+++ client.py
@@ -1,9 +1,14 @@
+from pickle_data import get_event
+from pickle_data import HEADERSIZE
+from hashlib import sha1
+from string import hexdigits
 import pickle
 import socket
 import threading
-from pickle_data import get_event
-from pickle_data import HEADERSIZE
+import time
 
+from crc_func import *
+from Packet import *
 
 try:
     sock_fd  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Initialization a UDP socket
@@ -22,27 +27,8 @@ except socket.error as err:          #Catching any error or exception thrown dur
     print("[-] Error failed to bind the socket")
     raise
 
-
-# addr = raw_input('[*] Enter the address of peer :') #Taking peer_address as input from userv
-# port = int(input('[*] Enter the port of peer : '))  #Takign peer_port as input from user
-
-
-# msg = sock_fd.recv(1024).decode()   #Receiving UDP packet from any address send to m_port 
-
-
-# print('[+] Peer msg {}'.format(msg))
-
-# sock_fd.sendto(b'\n[+] Hi from peer2', (addr, port))
-
-
-
-
-
-# print("[+] Connected to peer")
-# print("[*] Address : {}".format(addr))
-# print("[*] Port : {}".format(port))
-
-#Subroutine for the thread which listen for any incoming socket connection 
+#Subroutine for the thread which listen for any incoming socket connection
+ack_recieved = False
 def l_subroutine():
     try:
         sock_fd  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #Initialization a UDP socket
@@ -51,25 +37,39 @@ def l_subroutine():
         print("[-] Error failed to create socket")
         raise
 
-    try:
-        sock_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   #Setting socket option to reuse the addresss
-        try:
-            sock_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)   #Setting socket option to reuse the port
-        except AttributeError:
-            print("[-] Error setting option")
-            raise
-    except socket.error as err:
-        print("[-] Error setting option") 
-        raise
-
     sock_fd.bind(('0.0.0.0', m_port)) #Binding the socket to port m_port
+
+    expected_seq_num = 0
+
     while True:
-        msg = sock_fd.recv(1024).decode()
-        # print(pickle.loads(msg[HEADERSIZE:]))
-        print("[+]peer_msg: {}".format(pickle.loads(msg)))
 
+        package = sock_fd.recv(1024)
+       
+        G = "10011" #same G on both peers
+        R = decoder(package, G)
+        # temp = "0" * (len(G) - 1) #if the remainder is all zeros 
+        # if R == temp:
+        #     print("Remainder " + R + " no errors found")
+        # else:
+        #     print("Remainder= " + R + " errors detected")
+        # print(R + " errors detected")
 
-    
+        rcv_packet = pickle.loads(package)
+       # print(rcv_packet)
+        if rcv_packet.type == 'has_data':
+            if rcv_packet.check() and expected_seq_num == rcv_packet.seq_num:
+                package = Packet_reciever('ACK', rcv_packet.seq_num)
+                packet = pickle.dumps(package)
+
+                sock_fd.sendto(packet, (rcv_packet.ip, int(rcv_packet.port)))
+                
+                print("[+] peer_msg: {}".format(rcv_packet.data))
+        
+        elif rcv_packet.type == 'has_ack':
+            global ack_recieved
+            ack_recieved = True
+            print("     - Delivered")
+            expected_seq_num = 1 - expected_seq_num
 
 
 s_thread = threading.Thread(target=l_subroutine)    #Creating a thread for listening on the port
@@ -84,29 +84,39 @@ except socket.error as err:     # Catch any errors or exception thrown during in
     print("[-] Error failed to create socket")
     raise
 
-try:
-    sock_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   #Setting socket option to reuse the addresss
-    try:
-        sock_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)   #Setting socket option to reuse the port
-    except AttributeError:
-        print("[-] Error setting option")
-        raise
-except socket.error as err:
-    print("[-] Error setting option")    
-    raise
+
+# sender's code
 
 while True:
-    msg = raw_input('> ')
+    msg = raw_input('[>] ')
     addr = raw_input('[*] Enter the address of peer :') #Taking peer_address as input from userv
     port = int(input('[*] Enter the port of peer : '))  #Takign peer_port as input from user
 
+    sequence_number = 0
+    ack_recieved = False
+    num_of_tries = 0
 
-    if msg == "EVENT":
-        pkl_data = get_event()
-        sock_fd.sendto(pkl_data, (addr, port))
-    else:
-        msg = pickle.dumps(msg)
-        # msg = bytes("{}".format(len(msg) < HEADERSIZE).encode('utf-8'))+msg
-        sock_fd.sendto(msg, (addr, port))
+    while not ack_recieved:
 
+        if msg == "EVENT":
+            pkl_data = get_event()
+            package = Packet_sender(pkl_data, sequence_number, '127.0.0.1', m_port)
+        else:   
+            package = Packet_sender(msg, sequence_number, '127.0.0.1', m_port)
+       
+        bin_data  = pickle.dumps(package) #covert string to binary CRC
+        G= "10011"                                           #four bit checker
+        package = encoder(bin_data,G)     
 
+        sock_fd.sendto(package, (addr, port))
+
+        time.sleep(1)
+        if(not ack_recieved):
+            if num_of_tries>10:
+                print("Many attempts unsuccessful, try again!")
+                break
+            else:
+                num_of_tries+=1
+                print("Retrying sending data packet...")
+            
+    sequence_number = 1 - sequence_number
